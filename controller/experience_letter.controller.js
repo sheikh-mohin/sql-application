4d33e37e80924cf9bdc64b1b02f95ebb496c596d
mohin <mohinsheikh2011@gmail.com>
2022-10-14 23:29:15 +0530

    remove unnecessary service

--- controller/experience_letter.controller.js
+++ controller/experience_letter.controller.js
@@ -1,46 +1,7 @@
-const experience_letterService = require("../database/experience_letter.db");
 const userService = require("../database/user.db");
 const { getResponseStructure } = require("../constants/response.structure");
 const { message, status } = require("../constants/response.constants");
 const { expLetter } = require("../service/generatePdf");
-
-exports.register = async (req, res) => {
-    try {
-        const jsonData = req.body;
-        const experience_letter = await experience_letterService.get(jsonData.id);
-        if (experience_letter.length > 0) {
-            return res
-                .status(status.conflict)
-                .send(
-                    getResponseStructure(
-                        status.conflict,
-                        "Experience letter " + message.alreadyExist
-                    )
-                );
-        }
-        const record = await experience_letterService.add({
-            ...jsonData,
-        });
-        if (record) {
-            return res
-                .status(status.successCreated)
-                .send(
-                    getResponseStructure(
-                        status.successCreated,
-                        "Experience letter " + message.createSuccess
-                    )
-                );
-        } else {
-            return res
-                .status(status.notfound)
-                .send(getResponseStructure(status.success, message.somethingWrong));
-        }
-    } catch (error) {
-        return res
-            .status(status.success)
-            .send(getResponseStructure(status.notfound, error.message.toString()));
-    }
-};
 
 exports.createExpLetter = async (req, res) => {
     try {
--- router/experience_letter.router.js
+++ router/experience_letter.router.js
@@ -1,6 +1,5 @@
 const express = require('express');
 express.Router();
-const validator = require('../validator/experience_letter.validator'); // Middleware For user validation
 const api = require('../controller/experience_letter.controller');
 
 module.exports = (router) => {
